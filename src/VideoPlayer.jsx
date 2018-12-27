92aacd9c2221aa18921697d928ff63c7b8373313
RemondJu <remond.neiluj@gmail.com>
2018-12-27 19:59:37 +0100

    ES lint code cleaning

--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -3,69 +3,85 @@ import Video from 'react-video-renderer';
 import './VideoPlayer.css';
 
 class VideoPlayer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() { 
-        return ( 
-          <div className="VideoPlayer">
-            <Video src="http://vjs.zencdn.net/v/oceans.mp4" controls preload="metadata">
-              {(video, state, actions) => {
-                const loadingComponent = state.isLoading ? 'loading...' : undefined;
-                
-                return(
-                <div className="Video" >
-                  <div className="videoReader" tabIndex="0" onKeyDown={(e) => {
-                      switch(e.which) {
-                        case 37: actions.navigate(state.currentTime - 0.04);
+  render() {
+    return (
+      <div className="VideoPlayer">
+        <Video src="http://vjs.zencdn.net/v/oceans.mp4" controls preload="metadata">
+          {(video, state, actions) => {
+            const loadingComponent = state.isLoading ? 'loading...' : undefined;
+            return (
+              <div className="Video">
+                <div
+                  className="videoReader"
+                  tabIndex="0"
+                  role="button"
+                  onKeyDown={(e) => {
+                    let keyDownHandler = null;
+                    switch (e.which) {
+                      case 37: keyDownHandler = actions.navigate(state.currentTime - 0.04);
                         break;
-                        case 39: actions.navigate(state.currentTime + 0.04);
+                      case 39: keyDownHandler = actions.navigate(state.currentTime + 0.04);
                         break;
-                        case 32: state.status === "paused" ? actions.play() : actions.pause();
+                      case 32: keyDownHandler = state.status === 'paused' ? actions.play() : actions.pause();
                         break;
-                        case 77: actions.toggleMute();
+                      case 77: keyDownHandler = actions.toggleMute();
                         break;
-                        case 70: actions.requestFullscreen();
+                      case 70: keyDownHandler = actions.requestFullscreen();
                         break;
-                        default: return "";
-                      };
+                      default: return '';
                     }
+                    return keyDownHandler;
                   }
-                  >
-                    {video}
-                  </div>                  
-                  {loadingComponent}
-                  <div>
-                    {Math.floor(state.currentTime)}s / {Math.floor(state.duration)}s
-                  </div>
-                  <div>
-                    Buffering:  {Math.floor(state.buffered * (100 / state.duration))}%
-                  </div>
-                  <progress value={state.currentTime} max={state.duration} onChange={actions.navigate} />
-                  <div className="videoControls">
-                    <div>
-                      <input type="range" value={state.volume} max={1} step="0.01" onChange={(e) => actions.setVolume(e.target.value)} />
-                      <p>Vol. {Math.floor(state.volume * 100)}%</p>
-                    </div>
-                    
-                    <button onClick={actions.toggleMute}>{state.isMuted ? "Unmute" : "Mute"}</button>
-                    <button onClick={state.status === "paused" ? actions.play : actions.pause}>{state.status === "paused" ? "Play" : "Pause"}</button>
-                    <div>
-                      <button className="frameButton" onClick={() => actions.navigate(state.currentTime - 0.04)} >{"<"}</button>
-                      <button className="frameButton" onClick={() => actions.navigate(state.currentTime + 0.04)} >></button>
-                    </div>                    
-                    <button onClick={actions.requestFullscreen}>Fullscreen</button>
-                  </div>
-                  
+                }
+                >
+                  {video}
                 </div>
-                );
-              }}
-            </Video>
-          </div>
-         );
-    }
+                {loadingComponent}
+                <div>
+                  {Math.floor(state.currentTime)}
+                  s /
+                  {Math.floor(state.duration)}
+                  s
+                </div>
+                <div>
+                  Buffering:
+                  {Math.floor(state.buffered * (100 / state.duration))}
+                  %
+                </div>
+                <progress
+                  value={state.currentTime}
+                  max={state.duration}
+                  onChange={actions.navigate}
+                />
+                <div className="videoControls">
+                  <div>
+                    <input type="range" value={state.volume} max={1} step="0.01" onChange={e => actions.setVolume(e.target.value)} />
+                    <p>
+                      Vol.
+                      {Math.floor(state.volume * 100)}
+                      %
+                    </p>
+                  </div>
+                  <button type="button" onClick={actions.toggleMute}>{state.isMuted ? 'Unmute' : 'Mute'}</button>
+                  <button type="button" onClick={state.status === 'paused' ? actions.play : actions.pause}>{state.status === 'paused' ? 'Play' : 'Pause'}</button>
+                  <div>
+                    <button type="button" className="frameButton" onClick={() => actions.navigate(state.currentTime - 0.04)}>{'<'}</button>
+                    <button type="button" className="frameButton" onClick={() => actions.navigate(state.currentTime + 0.04)}>{'>'}</button>
+                  </div>
+                  <button type="button" onClick={actions.requestFullscreen}>Fullscreen</button>
+                </div>
+              </div>
+            );
+          }}
+        </Video>
+      </div>
+    );
+  }
 }
- 
+
 export default VideoPlayer;
